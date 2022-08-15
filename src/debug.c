d5f6a005bb46dfb0a5fe56c71165b53da896028f
Pjiwm <pim.munne@gmail.com>
2022-08-15 22:45:56 +0200

    added value arrays

--- src/debug.c
+++ src/debug.c
@@ -2,6 +2,8 @@
 
 #include <stdio.h>
 
+#include "value.h"
+
 void disassembleChunk(Chunk* chunk, const char* name) {
   printf("== %s ==\n", name);
   for (int offset = 0; offset < chunk->count;) {
@@ -10,14 +12,24 @@ void disassembleChunk(Chunk* chunk, const char* name) {
 }
 
 static int simpleInstruction(const char* name, int offset) {
-  printf(" %s\n", name);
+  printf("%s\n", name);
   return offset + 1;
 }
 
+static int constantInstruction(char* name, Chunk* chunk, int offset) {
+  uint8_t constant = chunk->code[offset + 1];
+  printf("%-8s %4d", name, constant);
+  printValue(chunk->constants.values[constant]);
+  printf("\n"); 
+  return offset + 2;
+}
+
 int disassembleInstruction(Chunk* chunk, int offset) {
-  printf("%04d", offset);
+  printf("%04d ", offset);
   uint8_t instruction = chunk->code[offset];
   switch (instruction) {
+    case OP_CONSTANT:
+      return constantInstruction("OP_CONSTANT", chunk, offset);
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
     default:
