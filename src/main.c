487759c8a842625a906b4d93b78475894a805a88
Pjiwm <pim.munne@gmail.com>
2022-08-17 01:56:38 +0200

    made start on VM

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) { 
+  initVM();
+  
   Chunk chunk;
   initChunk(&chunk);
   int constant = addConstant(&chunk, 1.2);
@@ -10,5 +13,7 @@ int main(int argc, const char* argv[]) {
   writeChunk(&chunk, constant, 123);
   writeChunk(&chunk, OP_RETURN, 123);
   disassembleChunk(&chunk, "test chunk");
+  interpreter(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0; }
