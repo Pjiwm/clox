c3886e10bdbfce1fcac4f557dd60ea73aa564e7c
Pjiwm <pim.munne@gmail.com>
2022-08-17 03:46:14 +0200

    VM can handle arithmetic operations

--- src/debug.c
+++ src/debug.c
@@ -18,16 +18,16 @@ static int simpleInstruction(const char* name, int offset) {
 
 static int constantInstruction(char* name, Chunk* chunk, int offset) {
   uint8_t constant = chunk->code[offset + 1];
-  printf("%-8s %4d", name, constant);
+  printf("%-12s %4d", name, constant);
   printValue(chunk->constants.values[constant]);
-  printf("\n"); 
+  printf("\n");
   return offset + 2;
 }
 
 int disassembleInstruction(Chunk* chunk, int offset) {
   printf("%04d ", offset);
-  if (offset > 0 && chunk->lines[offset] == chunk->lines[offset -1]) {
-    printf("  | ");
+  if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
+    printf("   | ");
   } else {
     printf("%4d ", chunk->lines[offset]);
   }
@@ -35,6 +35,16 @@ int disassembleInstruction(Chunk* chunk, int offset) {
   switch (instruction) {
     case OP_CONSTANT:
       return constantInstruction("OP_CONSTANT", chunk, offset);
+    case OP_ADD:
+      return simpleInstruction("OP_ADD", offset);
+    case OP_SUBTRACT:
+      return simpleInstruction("OP_SUBTRACT", offset);
+    case OP_MULTIPLY:
+      return simpleInstruction("OP_MULTIPLY", offset);
+    case OP_DIVIDE:
+      return simpleInstruction("OP_DIVIDE", offset);
+    case OP_NEGATE:
+      return simpleInstruction("OP_NEGATE", offset);
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
     default:
--- src/main.c
+++ src/main.c
@@ -1,19 +1,32 @@
-#include "common.h"
 #include "chunk.h"
+#include "common.h"
 #include "debug.h"
 #include "vm.h"
 
-int main(int argc, const char* argv[]) { 
+int main(int argc, const char* argv[]) {
   initVM();
-  
+
   Chunk chunk;
   initChunk(&chunk);
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-  writeChunk(&chunk, OP_RETURN, 123);
+  int constant = addConstant(&chunk, 1);
+  writeChunk(&chunk, OP_CONSTANT, 1);
+  writeChunk(&chunk, constant, 1);
+
+  constant = addConstant(&chunk, 2);
+  writeChunk(&chunk, OP_CONSTANT, 1);
+  writeChunk(&chunk, constant, 1);
+  writeChunk(&chunk, OP_MULTIPLY, 1);
+
+  constant = addConstant(&chunk, 1);
+  writeChunk(&chunk, OP_CONSTANT, 1);
+  writeChunk(&chunk, constant, 1);
+  writeChunk(&chunk, OP_ADD, 1);
+
+  writeChunk(&chunk, OP_RETURN, 1);
+
   disassembleChunk(&chunk, "test chunk");
   interpret(&chunk);
   freeVM();
   freeChunk(&chunk);
-  return 0; }
+  return 0;
+}
