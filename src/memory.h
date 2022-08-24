01c0c72fac8b1f30126ed61364fa97875379ba81
Pjiwm <pim.munne@gmail.com>
2022-08-25 01:45:14 +0200

    added strings

--- src/memory.h
+++ src/memory.h
@@ -2,15 +2,16 @@
 #define clox_memory_h
 #include "common.h"
 
-#define GROW_CAPACITY(capacity) \
-    ((capacity) < 8 ? 8 : (capacity) * 2)
+#define ALLOCATE(type, count) (type*)reallocate(NULL, 0, sizeof(type) * (count))
+
+#define GROW_CAPACITY(capacity) ((capacity) < 8 ? 8 : (capacity)*2)
 
 #define FREE_ARRAY(type, pointer, oldCount) \
-    reallocate(pointer, sizeof(type) * (oldCount), 0)
+  reallocate(pointer, sizeof(type) * (oldCount), 0)
 
-#define GROW_ARRAY(type, pointer, oldCount, newCount) \
-    (type*)reallocate(pointer, sizeof(type) * (oldCount), \
-    sizeof(type) * (newCount))
+#define GROW_ARRAY(type, pointer, oldCount, newCount)   \
+  (type*)reallocate(pointer, sizeof(type) * (oldCount), \
+                    sizeof(type) * (newCount))
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
 
